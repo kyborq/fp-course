20db3d979ddf074211335a31d97020e65d0d8aab
Konstantin <k.podyganov@mail.ru>
2024-01-24 09:04:38 +0300

    fix(lw1): refactored triangle function

--- lw1/lw14.hs
+++ lw1/lw14.hs
@@ -1,6 +1,6 @@
 module Main where
 
-triangle x = if x <= 0 then 0 else x + triangle (x - 1)
+triangle x = x * (x + 1) `div` 2
 
 triangles x = if x <= 0 then [] else triangle x : triangles (x - 1)
 
